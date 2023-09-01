6376a496301056f015f413503d5a455eda057ae1
Ethan Phoenix <ethanjessephoenix@gmail.com>
2023-09-01 14:52:41 -0500

    prettier

--- server/routes/itinerary.routes.js
+++ server/routes/itinerary.routes.js
@@ -1,9 +1,11 @@
 const ItineraryController = require('../controllers/itinerary.controller');
+const {authenticate} = require('../config/jwt.config');
+
 module.exports = (app) => {
     app.get('/api', ItineraryController.index);
-    app.post('/api/itinerary', ItineraryController.createItinerary);
-    app.get('/api/itinerary', ItineraryController.getAllItineraries); 
-    app.get('/api/itinerary/:id', ItineraryController.getItinerary);
-    app.put('/api/itinerary/:id', ItineraryController.updateItinerary);
-    app.delete('/api/itinerary/:id', ItineraryController.deleteItinerary);
+    app.post('/api/itinerary', authenticate, ItineraryController.createItinerary);
+    app.get('/api/itinerary', authenticate, ItineraryController.getAllItineraries); 
+    app.get('/api/itinerary/:id', authenticate, ItineraryController.getItinerary);
+    app.put('/api/itinerary/:id', authenticate, ItineraryController.updateItinerary);
+    app.delete('/api/itinerary/:id', authenticate, ItineraryController.deleteItinerary);
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
-app.use(cors());
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 require('./config/mongoose.config');
+require('dotenv').config();
 require('./routes/itinerary.routes')(app);
+require('./routes/user.routes')(app);
     
 app.listen(8000, () => {
     console.log("Listening at Port 8000")
